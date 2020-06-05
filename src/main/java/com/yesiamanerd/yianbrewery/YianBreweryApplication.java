70c6b7da38deab772c4c71a1a05b269d3aa3c822
Geoffrey Brophy <yesiamanerd@gmail.com>
2020-06-04 20:15:36 -0400

    First Commit

---
+++ src/main/java/com/yesiamanerd/yianbrewery/YianBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.yesiamanerd.yianbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YianBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YianBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yesiamanerd/yianbrewery/YianBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesiamanerd.yianbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YianBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
